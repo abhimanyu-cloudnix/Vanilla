d83ab4d3166817dc09509ea06f838539061c6a69
abhimanyu-cloudnix <abhimanyu@cloudnix.com>
2019-09-11 10:02:22 +0530

    Scroll changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
 // getter for full name
 var user = {
-	firstname: "Abhi",
-	lastname : "Kr",
+	firstname: "",
+	lastname : "",
 	get fullName() {
-	return this.firstname + " " + this.lastname;
+		if(this.firstname == "" || this.lastname == ""){
+			return;
+		}else{
+			return this.firstname + " " + this.lastname;
+		}
 	}
 };
 
+
+// creating post
 function create_post(id, post){
 	var username = user.fullName;
 
-	var post = '<div class="active-post-block post-'+id+'" postID="'+id+'"><div class="active-post-header"><i class="fa fa-user-circle-o active-user-image" aria-hidden="true"></i><span class="posted-user">'+username+'</span></div><div class="active-post-content"><div class="image-block" id="post-image"></div><div class="text-block"><div id="post-texts">'+post+'</div></div></div><div class="active-post-footer"><button id="post-comment-btn" class="comment-call-'+id+'" postID="'+id+'" onclick="open_comments(this)"><i class="fa fa-comments" aria-hidden="true"></i>Comment</button></div><div class="active-post-comment-block slider slider-comment-'+id+'" style="display: none" id="show-comments-'+id+'"><input type="text" name="comments" placeholder="Enter your comment..." id="user-comment-'+id+'" class="user-comment-input"><button class="submit_comment" onclick="submit_comment('+id+')">submit</button></div></div>'
+	var post = '<div class="active-post-block post-'+id+'" postID="'+id+'"><div class="active-post-header"><i class="fa fa-user-circle-o active-user-image" aria-hidden="true"></i><span class="posted-user">'+username+'</span></div><div class="active-post-content"><div class="text-block"><div id="post-texts">'+post+'</div></div></div><div class="active-post-footer"><button id="post-comment-btn" class="comment-call-'+id+'" postID="'+id+'" onclick="open_comments(this)"><i class="fa fa-comments" aria-hidden="true"></i>Comment</button></div><div class="active-post-comment-block slider slider-comment-'+id+'" style="display: none" id="show-comments-'+id+'"><input type="text" name="comments" placeholder="Enter your comment..." id="user-comment-'+id+'" class="user-comment-input" onkeyup="call_submit_comment('+id+')"><button class="submit_comment" onclick="submit_comment('+id+')">submit</button></div></div>'
 	return post ;
 }
 
+// creating comments
 function create_comment(id, comment, postID){
 	var username = user.fullName;
 
-	var post = '<div class="feed-comments" id="feed-comment-'+id+'" commentID="'+id+'"><div class="feed-comments-header"><i class="fa fa-user-circle-o active-user-image" aria-hidden="true"></i><span class="feed-comments-user">'+username+'</span></div><div class="feed-comments-text"><span class="feed-comments-text">'+comment+'</span></div><div class="footer-comments"><button class="submit_comment_reply" id="submit_comment_reply-'+id+'" onclick="submit_comment_reply('+id+')"><i class="fa fa-reply-all" aria-hidden="true"></i>Reply</button><div class="reply-section" id="reply-section-'+id+'" style="display:none"><input type="text" name="reply" placeholder="Enter your reply..." id="user-reply-'+id+'" class="user-reply-input"><button class="submit_reply" id="submit_reply-'+id+'" onclick="submit_reply('+id+')">Reply Now</button></div></div></div>'
+	var post = '<div class="feed-comments" id="feed-comment-'+id+'" commentID="'+id+'"><div class="feed-comments-header"><i class="fa fa-user-circle-o active-user-image" aria-hidden="true"></i><span class="feed-comments-user">'+username+'</span></div><div class="feed-comments-text"><span class="feed-comments-text">'+comment+'</span></div><div class="footer-comments"><button class="submit_comment_reply" id="submit_comment_reply-'+id+'" onclick="submit_comment_reply('+id+')"><i class="fa fa-reply-all" aria-hidden="true"></i>Reply</button><div class="reply-section" id="reply-section-'+id+'" style="display:none"><input type="text" name="reply" placeholder="Enter your reply..." id="user-reply-'+id+'" class="user-reply-input" onkeyup="call_submit_reply('+id+')" ><button class="submit_reply" id="submit_reply-'+id+'" onclick="submit_reply('+id+')">Reply Now</button></div></div></div>'
 	return post ;
 }
 
+// creating reply
 function create_reply(id, reply, postID){
 	var username = user.fullName;
 
-	var reply = '<div class="feed-reply" id="feed-reply-'+id+'" commentID="'+id+'"><div class="feed-reply-block"><i class="fa fa-user-circle-o active-user-image" aria-hidden="true"></i><span class="feed-reply-user">'+username+'</span><span class="feed-reply-text">'+reply+'</span></div><div class="footer-reply"><button class="submit_replied_reply" id="submit_replied_reply-'+id+'" onclick="submit_replied_reply('+id+')"><i class="fa fa-reply-all" aria-hidden="true"></i>Reply</button><div class="reply-section" id="reply-section-'+id+'" style="display:none"><input type="text" name="reply" placeholder="Enter your reply..." id="user-reply-'+id+'" class="user-reply-input"><button class="submit_reply" id="submit_reply-'+id+'" onclick="submit_reply('+id+')">Reply Now</button></div></div></div>'
+	var reply = '<div class="feed-reply" id="feed-reply-'+id+'" commentID="'+id+'"><div class="feed-reply-block"><i class="fa fa-user-circle-o active-user-image" aria-hidden="true"></i><span class="feed-reply-user">'+username+'</span><span class="feed-reply-text">'+reply+'</span></div><div class="footer-reply"><button class="submit_replied_reply" id="submit_replied_reply-'+id+'" onclick="submit_replied_reply('+id+')"><i class="fa fa-reply-all" aria-hidden="true"></i>Reply</button><div class="reply-section" id="reply-section-'+id+'" style="display:none"><input type="text" name="reply" placeholder="Enter your reply..." id="user-reply-'+id+'" class="user-reply-input" onkeyup="call_submit_reply('+id+')"><button class="submit_reply" id="submit_reply-'+id+'" onclick="submit_reply('+id+')">Reply Now</button></div></div></div>'
 	return reply ;
 }
 
@@ -51,17 +59,17 @@ document.addEventListener('click', function (event) {
 			return;
 		}
 
+		// setting Username
 		user.firstname = fname;
 		user.lastname = lname;
 		username = user.fullName;
-		// Store data
-		sessionStorage.setItem('username', username);
+
 		document.getElementById("header-username").innerHTML= username;
 
 		document.getElementById("userswap-modal").style.display = "none";
 	}
 
-	// post
+	// Craeting a new post
 	if (event.target.matches('#create-post-btn')) {
 		var post = document.getElementById("create-input-field").value;
 		console.log(post);
@@ -71,36 +79,34 @@ document.addEventListener('click', function (event) {
 		}
 		username = user.fullName;
 
+		// getting last id
 		var is_class_exist = document.getElementsByClassName('active-post-block');
 		console.log(is_class_exist);
 		if (is_class_exist.length > 0) {
 			var id = parseInt(document.getElementById('post-add-anchor').lastChild.getAttribute("postID")); 
-  			console.log(id);
+  			// console.log(id);
   			id++;
-  		// 		document.getElementById("demo").innerHTML = x;
 		}else{
 			var id = 0;
 		}
 
 		var target = document.querySelector("#post-add-anchor");
+		// creating post with id
 		post_content = create_post(id, post);
 		target.innerHTML += post_content;
 
 		document.getElementById("create-input-field").value = "";
 
 	}
-
-	
 	
 
 }, false);
 
 
-// When page loads, modal is called
+// When page loads, modal is called 
 document.addEventListener('DOMContentLoaded', (event) => {
 	//the event occurred
 	username = user.fullName;
-
 
 	if( username == "" || username == null){
 		document.getElementById("userswap-modal").style.display = "block";
@@ -128,6 +134,7 @@ function submit_comment(id) {
 		}
 		username = user.fullName;
 
+		// getting last id
 		var is_class_exist = document.getElementById('show-comments-'+id);
 		var eleChild = is_class_exist.getElementsByClassName('feed-comments');
 		console.log(eleChild.length);
@@ -140,6 +147,7 @@ function submit_comment(id) {
 		console.log("id "+id);
 
 		console.log("postID - "+postID);
+		// creating comment with new id
 		var comment_text = create_comment(id, comments, postID);
 		var target = document.querySelector(".slider-comment-"+postID);
 
@@ -159,17 +167,17 @@ function submit_replied_reply(id){
 }
 
 
-//submit replu
+//submit reply
 function submit_reply(id) {
 	postID = id;
 	var reply = document.getElementById("user-reply-"+id).value;
-		console.log(reply);
 		if(reply == "" || reply == null){
 			alert("Please Enter some comment");
 			return;
 		}
 		username = user.fullName;
 
+		// getting last id
 		var is_class_exist = document.getElementById('reply-section-'+id);
 		var eleChild = is_class_exist.getElementsByClassName('feed-reply');
 		console.log(eleChild.length);
@@ -182,10 +190,26 @@ function submit_reply(id) {
 		console.log("id "+id);
 
 		console.log("postID - "+postID);
+		// creating reply with new id
 		var reply_text = create_reply(id, reply, postID);
 		var target = document.querySelector("#reply-section-"+postID);
 
 		target.innerHTML += reply_text;
 
 
+}
+
+
+// On enter click from Keyboard call submit comment
+function call_submit_comment(id){
+	if (event.keyCode === 13) {
+        submit_comment(id);
+    }
+}
+
+// On enter click from Keyboard call submit reply
+function call_submit_reply(id){
+	if (event.keyCode === 13) {
+        submit_reply(id);
+    }
 }
